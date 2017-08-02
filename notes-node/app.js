656f405a8f758305f6f5f0c3e14b1b25cb38dd04
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-03 11:13:49 +1200

    add Yarg as argment

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,21 +3,22 @@ console.log("starting app.");
 const fs = require('fs');
 const notes = require('./notes.js');
 const _ = require('lodash');
-var res = notes.addNote();
-console.log(res);
+const yargs = require('yargs');
 
-var command = process.argv[2];
-console.log(process.argv);
+const argv = yargs.argv;
+var command = argv._[0];
+// console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 console.log('Command', command);
-if(command ==='add') {
-  console.log('Adding new note'); 
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes');
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
 } else if (command === 'remove') {
-  console.log('Removing note');
-} else if (command === 'read'){
-   console.log('Reading note');
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized');
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,9 +1,25 @@
 console.log('Starting notes.js');
 //console.log(module);
 
-module.exports.age = 35;
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New Notes';
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
 };
 
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
