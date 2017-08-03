571d35ba3a6545205a182b3463d1cb0113e04af8
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-03 12:35:40 +1200

    remove note

--- notes-node/app.js
+++ notes-node/app.js
@@ -26,7 +26,9 @@ if (command === 'add') {
 } else if (command === 'read') {
   notes.getNote(argv.title);
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
 } else {
   console.log('Command not recognized');
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -39,7 +39,11 @@ var getNote = (title) => {
 };
 
 var removeNote = (title) => {
-  console.log('Removing note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+
+  return notes.length !== filteredNotes.length;
 };
 
 module.exports = {
