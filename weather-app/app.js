49c0bede0c8ad8ab48166f03b46f24e41042e9ad
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-06 14:18:31 +1200

    make callback api works

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,36 @@
 const request = require('request');
 
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadeiphia',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  //console.log(JSON.stringify(body, undefined, 2));
+  //console.log(JSON.stringify(response, undefined, 2));
+  //console.log(JSON.stringify(error, undefined, 2));
+  } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 }); 
