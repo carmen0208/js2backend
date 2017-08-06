4ec2d733ce6190ffd0f4801ddad8a14489b3c9ce
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-06 14:32:07 +1200

    refactoring and get callback function works

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,5 @@
-const request = require('request');
-
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -15,22 +14,11 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  //console.log(JSON.stringify(body, undefined, 2));
-  //console.log(JSON.stringify(response, undefined, 2));
-  //console.log(JSON.stringify(error, undefined, 2));
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.getcodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage) {
+    console.log(errorMessage);
+  } else {
+    //results in actually is a json or hash as well
+    console.log(JSON.stringify(results, undefined,2));
   }
-}); 
+});
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,26 @@
+const request = require('request');
+
+var getcodeAddress = (address, callback) => {
+  var encodedAddress = encodeURIComponent(address);
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      callback('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+      callback('Unable to find that address.');
+    //console.log(JSON.stringify(body, undefined, 2));
+    //console.log(JSON.stringify(response, undefined, 2));
+    //console.log(JSON.stringify(error, undefined, 2));
+    } else if (body.status === 'OK') {
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    }
+  });
+};
+
+module.exports.getcodeAddress = getcodeAddress;
