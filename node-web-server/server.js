947b3768fe0d65ad92679bd3173de0cf54af0ad0
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-15 22:48:53 +1200

    Setup start script and heroku port

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,31 @@
+const fs = require('fs');
+
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 //get html file
+
+//middle ware
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+//user middleware to set
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+    // next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', ()=> {
@@ -47,4 +66,6 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Running into an error :<'
     });
 });
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
