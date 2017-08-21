8767897066b46a0a886e9eef6a192a7dd4a2b354
Carmen Liu <carmen.liu@orionhealth.com>
2017-08-22 10:10:13 +1200

    update mongodb

---
+++ node-todo-api/playground/mondodb-update.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('599a16246e640585fec7c0b0')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('599a16246e640585fec7c0b0')
+  }, {
+    $set: {
+      name: 'Carmen Ninja'
+    },
+    //incremental the number and plus 1
+    //https://docs.mongodb.com/manual/reference/operator/update/inc/#up._S_inc
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
